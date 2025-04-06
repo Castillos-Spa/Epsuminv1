6f4a505df2dad6cd601fc5436c6a2ac0fbb907dd
Wils0nCastill0 <151974275+Wils0nCastill0@users.noreply.github.com>
2025-04-06 17:26:00 -0400

    Refactor code structure for improved readability and maintainability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import CategoriaProductos from "@/components/CategoriaProductos";
-import ProductosDestacados from "@/components/ProductosDestacados";
-import Catalogo from "@/components/Catalogo";
-import ServicioInfo from "@/components/ServicioInfo";
-import Maquinaria from "@/components/Maquinaria";
-import Testimonios from "@/components/Testimonios";
-import Noticias from "@/components/Noticias";
-import MarcasAsociadas from "@/components/MarcasAsociadas";
-import AcercaDeNosotros from "@/components/AcercaDeNosotros";
-import NuestrosObjetivos from "@/components/NuestrosObjetivos";
-import NuestrosValores from "@/components/NuestrosValores";
-import Contacto from "@/components/Contacto";
-import Footer from "@/components/Footer";
-=======
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 
@@ -30,18 +13,12 @@ import AcercaDeNosotros from "@/components/sections/about/AcercaDeNosotros";
 import NuestrosObjetivos from "@/components/sections/about/NuestrosObjetivos";
 import NuestrosValores from "@/components/sections/about/NuestrosValores";
 import Contacto from "@/components/sections/home/Contacto";
->>>>>>> 3b5ce78 (structure-1)
 
 export default function Home() {
   return (
     <main>
       <Navbar /> {/* Barra de Navegacion*/}
-<<<<<<< HEAD
-      <Hero/> {/* Seccion principal con fondo con imagen carrrusel */}
-      <CategoriaProductos/> {/* Seccion de categorias circulares de productos */}
-=======
       <Hero/> {/* Seccion principal con fondo con imagen carrusel */}
->>>>>>> 3b5ce78 (structure-1)
       <ProductosDestacados/> {/* seccion de productos destacados  */}
       <Catalogo/> {/* seccion de catalogo de productos y servicios */}
       <ServicioInfo/> {/* Seccion de necesita nuestros servicios */}
